0add3915d1a5e39e83d309b8705175158b7fecc8
Kanik Tyagi <kaniktyagi111@gmail.com>
2026-09-20 18:24:28 +0530

    Create Binary Search Tree

---
+++ Tree/Binary Search Tree/createBST.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root,int val) {
+    if(root == NULL) {
+        Node* temp = new Node(val);
+        return temp;
+    }
+
+    if(val < root->data) {
+        root->left = insert(root->left,val);
+    }
+    else {
+        root->right = insert(root->right,val);
+    }
+
+    return root;
+}
+
+int main() {
+    int arr[] = {3,4,8,5,7,10};
+
+    Node* root = NULL;
+    for(int i=0; i<6; i++) {
+        root = insert(root,arr[i]);
+    }
+
+    return 0;
+}
